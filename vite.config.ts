c64d3977aecd08cb9852d2adc2f996acaa5043f0
Richard <richard.chen.io@proton.me>
2024-06-27 22:07:48 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // base: "/bmi-calculator/",
+  base: "/bmi-calculator/",
   plugins: [react()],
   resolve: {
     alias: {
